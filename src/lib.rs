6f9efd692ac615268771f7dde39b0ec638649973
Hector <agenthector007@gmail.com>
2025-01-31 12:22:10 +0100

    readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use util::get_document;
+use util::{get_document, replace_text};
 use wasm_bindgen::prelude::*;
 use web_sys::{window, Document, Element};
 #[macro_use]
@@ -7,26 +7,15 @@ mod util;
 #[wasm_bindgen(start)]
 pub async fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    log!("Init");
     let selector = ".detailsActionScroll .details-log-message .ng-binding";
-    log!("Hello from Rust");
-    make_the_window_small();
 }
 
 #[wasm_bindgen]
-pub fn reformat_div(selector: &str, new_text: &str) {
+pub fn replace_text(selector: &str, new_text: &str) {
     let document = get_document();
 
     if let Some(element) = document.query_selector(selector).unwrap() {
         element.set_inner_html(new_text);
     }
-}
-
-#[wasm_bindgen]
-pub fn make_the_window_small() {
-    // Resize the window to 500px by 500px.
-    let window = web_sys::window().unwrap();
-    window
-        .resize_to(500, 500)
-        .expect("could not resize the window");
-    log!("window didnt get smaller?");
 }
