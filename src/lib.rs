12709e0ef378771f370a95addc06124f9c86bec0
Hector <75610292+HectorSchreiner@users.noreply.github.com>
2025-02-21 18:17:48 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use parser::*;
 use wasm_bindgen::prelude::*;
+use web_sys::{Document, MutationObserver, MutationObserverInit};
 #[macro_use]
 mod util;
 mod config;
@@ -8,7 +9,7 @@ mod parser;
 #[wasm_bindgen(start)]
 pub async fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    parse_text(".detailsActionScroll.details-log-message.ng-binding").unwrap();
+    parse_text(".detailsActionsScroll.details-log-message.ng-binding").unwrap();
 }
 
 #[wasm_bindgen]
@@ -20,13 +21,26 @@ pub fn parse_text(selector: &str) -> Result<(), JsValue> {
 
     // debug log
     if let Some(element) = document.query_selector(selector).ok().flatten() {
-        log!("{:?}", element.inner_html());
-        let log_message_parser = LogMessageParser::new(element.inner_html());
-        let log_message_parser = log_message_parser.json_format().format_config_rules();
-        let formatted_text = format!("<pre>{}</pre>", log_message_parser.get_text());
+        log!("Here is the html: {:?}", element.inner_html());
+        let log_message_parser: LogMessageParser<Formatted> = LogMessageParser::new(element.inner_html());
+        //let log_message_parser = log_message_parser.json_format().format_config_rules();
+        let formatted_text = format!("hector was here{:?}", log_message_parser.get_text());
         log!("{:?}", formatted_text);
-        element.set_inner_html(&formatted_text);
+        element.set_text_content(Some(&formatted_text));
     }
 
     Ok(())
+}
+
+#[wasm_bindgen]
+pub fn observe_dom_changes(selector: &str) -> Result<(), JsValue> {
+    let document = web_sys::window()
+        .ok_or("Failed to get window")?
+        .document()
+        .ok_or("Failed to get document")?;
+
+        // let mutation_observer = new observer
+        // if let Some(element) = document.query_selector(selector).ok().flatten() {
+
+        // }
 }
